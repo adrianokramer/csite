4e7d8a7c2f17bb56493a7744f237fe0ef4dc5330
adrianok <adrianokramer@gmail.com>
2017-05-02 17:17:24 -0300

    Add Altair.

---
+++ altair_v1.2.0/admin/src/assets/js/pages/page_todo.js
@@ -0,0 +1,23 @@
+/*
+ *  Altair Admin
+ *  page_todo.js (page_todo.html)
+ */
+
+$(function() {
+    // tasks list
+    altair_todo.init();
+});
+
+altair_todo = {
+    init: function() {
+        var $todo_list = $('#todo_list');
+        $todo_list.find('input:checkbox')
+            .on('ifChecked', function(){
+                $(this).closest('li').addClass('md-list-item-disabled');
+            })
+            .on('ifUnchecked', function(){
+                $(this).closest('li').removeClass('md-list-item-disabled');
+            });
+    }
+};
+
